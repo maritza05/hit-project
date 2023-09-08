415a2e90b57502c5726a54ba5299cff7c9a8211b
Maritza <maritza@pop-os.localdomain>
2023-09-08 00:18:55 -0600

    Clean command line flags

--- cmd/hit/hit.go
+++ cmd/hit/hit.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
+	"runtime"
 )
 
-const (
-	bannerText = `
+const bannerText = `
  __  __     __     ______
 /\ \_\ \   /\ \   /\__  _\
 \ \  __ \  \ \ \  \/_/\ \/
@@ -14,23 +14,16 @@ const (
   \/_/\/_/   \/_/     \/_/
 `
 
-	usageText = `
-	Usage:
-	- url: HTTP server URL to make requests (required)
-	- n: Number of requests to make
-	- c: Concurrency level`
-)
-
 func banner() string { return bannerText[1:] }
-func usage() string { return usageText[1:] }
 
 func main() {
-	var f flags
+	f := &flags{
+		n: 100,
+		c: runtime.NumCPU(),
+	}
 	if err := f.parse(); err != nil {
-		fmt.Println(usage())
-		log.Fatal(err)
+		os.Exit(1)
 	}
 	fmt.Println(banner())
 	fmt.Printf("Making %d requests to %s with a concurrency level of %d.\n", f.n, f.url, f.c)
-	fmt.Println(usage())
 }
